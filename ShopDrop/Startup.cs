f18642aa1477622d43de79a5051edbddb7623214
Dario999 <darioivanovski33@gmail.com>
2020-09-04 15:28:28 +0200

    First commit

---
+++ ShopDrop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopDrop.Startup))]
+namespace ShopDrop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
