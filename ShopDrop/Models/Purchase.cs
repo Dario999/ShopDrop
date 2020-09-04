00e0784233f7a0ce53637d279ca842cef2a88e34
Dario999 <darioivanovski33@gmail.com>
2020-09-04 19:56:10 +0200

    purchase and additional user info added

---
+++ ShopDrop/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopDrop.Models
+{
+    public class Purchase
+    {
+
+        [Key]
+        public int id { get; set; }
+        public string user_id { get; set; }
+        public int product_id { get; set; }
+
+    }
+}
---
+++ ShopDrop/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopDrop.Models
+{
+    public class User
+    {
+
+        [Key]
+        private string id { get; set; }
+        private ICollection<Product> products { get; set; }
+        private ICollection<Purchase> purchases { get; set; }
+
+        public User()
+        {
+            products = new List<Product>();
+            purchases = new List<Purchase>();
+        }
+    }
+}
