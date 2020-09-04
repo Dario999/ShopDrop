c07c632751f9f77fb235a7113979e24d300b5b5f
Dario999 <darioivanovski33@gmail.com>
2020-09-04 19:58:04 +0200

    product model changed

--- ShopDrop/Models/Product.cs
+++ ShopDrop/Models/Product.cs
@@ -13,14 +13,11 @@ namespace ShopDrop.Models
         public string Name { get; set; }
         public float Price { get; set; }
         public int Quantity { get; set; }
-        public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
         public string Image { get; set; }
         public Category Category { get; set; }
-        public ICollection<Category> Categories { get; set; }
 
         public Product()
         {
-            ShoppingCarts = new List<ShoppingCart>();
         }
     }
 }
